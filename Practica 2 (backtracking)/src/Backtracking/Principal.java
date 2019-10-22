62015bd507faa3d4935f37dced0cd3f032df39ac
thehastag <thehastag@protonmail.com>
2019-10-22 16:57:29 +0200

    Añadida la practica de backtracking en la que tenemos que igualar una cadena de caracteres en el menor numero de movimientos posibles

---
+++ Practica 2 (backtracking)/src/Backtracking/Principal.java
@@ -0,0 +1,43 @@
+package Backtracking;
+
+public class Principal {
+
+    /**
+     * @param cadena1 cadena de virus inicial
+     * @param cadena2 cadena de virus que queremos copiar
+     * @return numero minimo de cambios que tenemos que realizar para que la cadena1 se transforme en la cadena2
+     */
+
+    public static int numMinMutaciones(String cadena1, String cadena2) {
+        return mejorNumMutaciones(cadena1, cadena2, 0,0, Integer.MAX_VALUE);
+    }
+
+    private static int mejorNumMutaciones(String s1, String s2, int pointer,int numMov, int mejorMov ){
+        if (s1.equals(s2)){
+            return Math.min(numMov, mejorMov);
+        }
+        else{
+            if((pointer < s2.length() && pointer < s1.length()) && s1.charAt(pointer) == s2.charAt(pointer)) //si son iguales los caracteres en la pos pointer los dejamos asi
+                mejorMov = Math.min(mejorNumMutaciones(s1,s2,pointer+1,numMov,mejorMov), mejorMov);
+            else{
+                if (s1.length() < s2.length()) { //inserta elem
+                    String aux = s1.substring(0, pointer) + s2.charAt(pointer) + s1.substring(pointer);
+                    mejorMov = Math.min(mejorNumMutaciones(aux, s2, pointer + 1, numMov + 1, mejorMov), mejorMov);
+                }
+
+                if (pointer < s1.length() && pointer < s2.length()) { //cambiar elem
+                    String aux = "";
+                    if(pointer == s1.length()-1) aux = s1.substring(0, pointer) + s2.charAt( pointer);
+                    else aux = s1.substring(0,  pointer) + s2.charAt( pointer) + s1.substring( pointer + 1);
+                    mejorMov = Math.min(mejorNumMutaciones(aux, s2,  pointer + 1, numMov + 1, mejorMov), mejorMov);
+                }
+
+                if (s1.length() > s2.length()){ //elimina elem
+                    String aux = s1.substring(0, pointer) + s1.substring(pointer + 1);
+                    mejorMov = Math.min(mejorNumMutaciones(aux, s2, pointer, numMov + 1, mejorMov),mejorMov);
+                }
+            }
+            return mejorMov;
+        }
+    }
+}
