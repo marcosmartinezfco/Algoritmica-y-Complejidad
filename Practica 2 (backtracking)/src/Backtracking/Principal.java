f722928118058df6d6bfe99513ba4eb6545d1337
thehastag <thehastag@protonmail.com>
2019-10-22 17:59:10 +0200

    Añadidas podas para mejorar los tiempos de respuesta, en el caso 11 se pasa de 125 segundos a 2 segundos

--- Practica 2 (backtracking)/src/Backtracking/Principal.java
+++ Practica 2 (backtracking)/src/Backtracking/Principal.java
@@ -13,31 +13,31 @@ public class Principal {
     }
 
     private static int mejorNumMutaciones(String s1, String s2, int pointer,int numMov, int mejorMov ){
-        if (s1.equals(s2)){
-            return Math.min(numMov, mejorMov);
-        }
-        else{
-            if((pointer < s2.length() && pointer < s1.length()) && s1.charAt(pointer) == s2.charAt(pointer)) //si son iguales los caracteres en la pos pointer los dejamos asi
+        if (s1.equals(s2))
+            mejorMov = Math.min(mejorMov, numMov);
+
+        else if (numMov < mejorMov){
+
+            if((pointer < s2.length() && pointer < s1.length()) && s1.charAt(pointer) == s2.charAt(pointer))
                 mejorMov = Math.min(mejorNumMutaciones(s1,s2,pointer+1,numMov,mejorMov), mejorMov);
-            else{
-                if (s1.length() < s2.length()) { //inserta elem
-                    String aux = s1.substring(0, pointer) + s2.charAt(pointer) + s1.substring(pointer);
-                    mejorMov = Math.min(mejorNumMutaciones(aux, s2, pointer + 1, numMov + 1, mejorMov), mejorMov);
-                }
 
-                if (pointer < s1.length() && pointer < s2.length()) { //cambiar elem
-                    String aux = "";
-                    if(pointer == s1.length()-1) aux = s1.substring(0, pointer) + s2.charAt( pointer);
-                    else aux = s1.substring(0,  pointer) + s2.charAt( pointer) + s1.substring( pointer + 1);
-                    mejorMov = Math.min(mejorNumMutaciones(aux, s2,  pointer + 1, numMov + 1, mejorMov), mejorMov);
-                }
-
-                if (s1.length() > s2.length()){ //elimina elem
-                    String aux = s1.substring(0, pointer) + s1.substring(pointer + 1);
-                    mejorMov = Math.min(mejorNumMutaciones(aux, s2, pointer, numMov + 1, mejorMov),mejorMov);
-                }
+            else if (pointer < s1.length() && pointer < s2.length()) { //cambiar elem
+                String aux = "";
+                if(pointer == s1.length()-1) aux = s1.substring(0, pointer) + s2.charAt(pointer);
+                else aux = s1.substring(0, pointer) + s2.charAt(pointer) + s1.substring(pointer + 1);
+                mejorMov = Math.min(mejorNumMutaciones(aux, s2, pointer + 1, numMov + 1, mejorMov), mejorMov);
             }
-            return mejorMov;
+
+            if (s1.length() < s2.length()) { //inserta elem
+                String aux = s1.substring(0, pointer) + s2.charAt(pointer) + s1.substring(pointer);
+                mejorMov = Math.min(mejorNumMutaciones(aux, s2, pointer + 1, numMov + 1, mejorMov), mejorMov);
+            }
+
+            if (s1.length() > s2.length()){ //elimina elem
+                String aux = s1.substring(0, pointer) + s1.substring(pointer + 1);
+                mejorMov = Math.min(mejorNumMutaciones(aux, s2, pointer, numMov + 1, mejorMov),mejorMov);
+            }
         }
+        return mejorMov;
     }
 }
