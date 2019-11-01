0eb99ea5ed612e1163efba794fa6aa8c9c0f5c61
thehastag <thehastag@protonmail.com>
2019-11-01 19:13:17 +0100

    Primer problema de los ejercicios resueltos de backtracking de examenes anteriores

---
+++ Ejercicios/src/CaminoMasLargo/CaminoMasLargo.java
@@ -0,0 +1,35 @@
+package CaminoMasLargo;
+
+public class CaminoMasLargo {
+    public  static int caminoMasLargo(int[][] grafo, int origen, int destino){
+        boolean[] visitados = new  boolean[grafo.length];
+        return caminoMasLargoAux(grafo, destino, visitados, origen,0,0);
+    }
+
+    private static int caminoMasLargoAux(int[][] grafo, int destino, boolean[] visitados, int nodo, int coste, int costeMax){
+        if(nodo == destino)
+            return  Math.max(coste, costeMax);
+        else{
+            for (int i = 0; i < visitados.length; i++)
+                if (!visitados[i] && grafo[nodo][i] != Integer.MAX_VALUE && i != nodo){
+                    int costeAux = caminoMasLargoAux(grafo,destino, visitados, i, coste+grafo[nodo][i], costeMax);
+                    if(costeAux > costeMax) {
+                        costeMax = costeAux;
+                        visitados[i] = true;
+                    }
+                }
+            return costeMax;
+        }
+    }
+
+    public static void main(String[] args){
+        int[][] grafo = {
+                {Integer.MAX_VALUE,1,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE},
+                {Integer.MAX_VALUE,Integer.MAX_VALUE,1,7,Integer.MAX_VALUE},
+                {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,1,Integer.MAX_VALUE},
+                {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,3},
+                {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE}
+        };
+        System.out.println("El camino mas largo se tarda: " + caminoMasLargo(grafo,0,4));
+    }
+}
