07503aee336fc5f8f29b881f309e6c42dc26b40c
thehastag <thehastag@protonmail.com>
2019-11-02 12:02:06 +0100

    Probado que el algoritmo funciona correctamente y modificado para que siga la estructura que aceptan en la asignatura

--- Ejercicios/src/CaminoMasLargo/CaminoMasLargo.java
+++ Ejercicios/src/CaminoMasLargo/CaminoMasLargo.java
@@ -2,28 +2,30 @@ package CaminoMasLargo;
 
 public class CaminoMasLargo {
     public  static int caminoMasLargo(int[][] grafo, int origen, int destino){
-        boolean[] visitados = new  boolean[grafo.length];
-        return caminoMasLargoAux(grafo, destino, visitados, origen,0,0);
+        return caminoMasLargoAux(grafo, destino, origen,0,0);
     }
 
-    private static int caminoMasLargoAux(int[][] grafo, int destino, boolean[] visitados, int nodo, int coste, int costeMax){
+    private static int caminoMasLargoAux(int[][] grafo, int destino, int nodo, int coste, int costeMax){
         if(nodo == destino)
             return  Math.max(coste, costeMax);
-        else{
-            for (int i = 0; i < visitados.length; i++)
-                if (grafo[nodo][i] != Integer.MAX_VALUE){
-                    int costeAux = caminoMasLargoAux(grafo,destino, visitados, i, coste+grafo[nodo][i], costeMax);
+        else {
+            int i = 0;
+            while (i < grafo.length){
+                if (grafo[nodo][i] != Integer.MAX_VALUE) {
+                    int costeAux = caminoMasLargoAux(grafo, destino, i, coste + grafo[nodo][i], costeMax);
                     costeMax = Math.max(costeAux, costeMax);
                 }
+                i++;
+            }
             return costeMax;
         }
     }
 
     public static void main(String[] args){
         int[][] grafo = {
-                {Integer.MAX_VALUE,1,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE},
+                {Integer.MAX_VALUE,1,Integer.MAX_VALUE,10,Integer.MAX_VALUE},
                 {Integer.MAX_VALUE,Integer.MAX_VALUE,1,7,Integer.MAX_VALUE},
-                {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,1,Integer.MAX_VALUE},
+                {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,1,5},
                 {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,3},
                 {Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE,Integer.MAX_VALUE}
         };
