dcf784b55fc0d54e98dc53482b4f02fb60021f1d
thehastag <thehastag@protonmail.com>
2019-11-14 20:25:48 +0100

    Devuleve una lista de objetos y los printea correctamente. Nos falta comporbar si es la solcuion optima o alguna cercana a ella y hacer mas pruebas

--- Practica 3 (voraces)/src/Principal.java
+++ Practica 3 (voraces)/src/Principal.java
@@ -11,6 +11,7 @@ public class Principal {
             if(esCandidatoFactible(candidato, inventario))
                 solucion.add(candidato);
         }
+
         return solucion;
     }
 
@@ -20,11 +21,52 @@ public class Principal {
         return aux;
     }
 
+    private static boolean esCandidatoFactible(Objeto candidato, Inventario inventario) {
+        for (int j = 0; j < inventario.getN(); j++) {
+            for (int i = 0; i < inventario.getN(); i++) {
+                if (inventario.getCelda(i,j) == -1 && celdaAceptable(i,j,candidato,inventario)){
+                    insertarObjeto(inventario, i , j , candidato);
+                    return true;
+                }
+            }
+        }
 
-    /**
-     * Ordenar solo por valor
-     * @param candidatos lista de objetos que queremos considerar para insertar en el inventario
-     */
+        return false;
+    }
+
+    private static boolean celdaAceptable(int fil, int col, Objeto objeto, Inventario inventario) {
+        int ancho = 0;
+        int largo = 0;
+
+        for (int i = fil; i < inventario.getN(); i++) {
+            if (inventario.getCelda(i, col) == -1)
+                ancho ++;
+            else
+                break;
+        }
+        for (int i = col; i < inventario.getN(); i++) {
+            if (inventario.getCelda(fil, i) == -1)
+                largo ++;
+            else
+                break;
+        }
+        return (objeto.getAlto() <= largo && objeto.getAncho() <= ancho);
+    }
+
+    private static void insertarObjeto(Inventario inventario, int i ,int j, Objeto candidato){
+        int auxj = j;
+        int auxi = i;
+
+        while (auxi < candidato.getAncho() + i){
+            while (auxj < candidato.getAlto() + j){
+                inventario.setCelda(auxi, auxj, candidato.getId());
+                auxj++;
+            }
+            auxj = j;
+            auxi++;
+        }
+    }
+
     private static void mergeSort(ArrayList<Objeto> candidatos){
         mergeSortAux(candidatos, 0, candidatos.size()-1);
     }
@@ -53,59 +95,9 @@ public class Principal {
                 aux.add(candidatos.get(d));
                 d++;
             }
-        for (int a = i; a <= k ; a++) { aux.add(candidatos.get(a)); }
-        for (int b = d; b <= in ; b++) { aux.add(candidatos.get(b)); }
+        for (int a = i; a <= k ; a++) aux.add(candidatos.get(a));
+        for (int b = d; b <= in ; b++) aux.add(candidatos.get(b));
         for (int c = 0; c < aux.size() ; c++) candidatos.set(c + i0, aux.get(c));
     }
 
-	private static boolean esCandidatoFactible(Objeto candidato, Inventario inventario) {
-        int [] celdasPosibles = posicion(inventario);
-        for (int i = 0; i < inventario.getN(); i++) {
-            for (int j = celdasPosibles[i]; j < inventario.getN(); j++) {
-                if(inventario.getCelda(j,i) == -1 && celdaAceptable(i,j,candidato,inventario)){
-                    insertarObjeto(inventario, candidato, i,j);
-                    return true;
-                }
-            }
-        }
-		return false;
-	}
-
-    /**
-     * Devulve un array con la celda libre mas a la izquierda de cada
-     * fila del inventario
-     */
-	private static int[] posicion(Inventario inventario){
-	    int[] celdasPosibles = new int[inventario.getN()];
-        for (int f = 0; f < inventario.getN(); f++)
-            for (int c = 0; c < inventario.getN() ; c++)
-                if(inventario.getCelda(c, f) == -1)
-                    celdasPosibles[f] = c;
-        return celdasPosibles;
-    }
-
-    private static boolean celdaAceptable(int fil, int col, Objeto objeto, Inventario inventario) {
-        int largo = 0;
-        int ancho = 0;
-
-        for (int i = fil; i < inventario.getN(); i++) {
-            if (inventario.getCelda(col, i) == -1)
-                ancho ++;
-            else
-                break;
-        }
-        for (int i = col; i < inventario.getN(); i++) {
-            if (inventario.getCelda(i, fil) == -1)
-                largo ++;
-            else
-                break;
-        }
-        return objeto.getAlto() <= largo && objeto.getAncho() <= ancho;
-    }
-
-    private static void insertarObjeto(Inventario inventario, Objeto objeto, int fil, int col){
-        for (int i = fil; i < objeto.getAlto()+fil-1 ; i++)
-            for (int j = col; j < objeto.getAncho()+col-1; j++)
-                inventario.setCelda(j,i,objeto.getId());
-    }
 }
