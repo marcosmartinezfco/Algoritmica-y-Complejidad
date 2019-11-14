a6250ddf685622c7d645c56943cdc7a027eb3d51
thehastag <thehastag@protonmail.com>
2019-11-14 23:26:45 +0100

    Añadidos comentarios al programa

--- Practica 3 (voraces)/src/Principal.java
+++ Practica 3 (voraces)/src/Principal.java
@@ -2,25 +2,44 @@ import java.util.*;
 
 public class Principal {
 
+    /**
+     * Funcion que partiendo de una lista de candidatos y un inventario rellena el mismo con
+     * el mayor numero de objetos posibles maximizando el valor total de los objetos insertados
+     * @param candidatos Array List con los posibles objetos a insertar en el inventario
+     * @param inventario Inventario donde se intentan insertar los objetos maximizando el valor total
+     * @return Array List (ordenado por valor) con los objetos que se han ordenado
+     */
     public static ArrayList<Objeto> llenarInventario(ArrayList<Objeto> candidatos, Inventario inventario) {
         ArrayList<Objeto> solucion = new ArrayList<>();
         mergeSort(candidatos);
-
+        
         while (0 < candidatos.size()){
             Objeto candidato = seleccionarCandidato(candidatos);
             if(esCandidatoFactible(candidato, inventario))
                 solucion.add(candidato);
         }
-
         return solucion;
     }
 
+    /**
+     * Funcion que selecciona el objeto con mas valor de un Array list, ordenado
+     * por valo,r de Objetos
+     * @param candidatos Array list ordenado de objetos segun su valor
+     * @return El objeto con mayor valor
+     */
     private static Objeto seleccionarCandidato(ArrayList<Objeto> candidatos) {
         Objeto aux = candidatos.get(0);
         candidatos.remove(0);
         return aux;
     }
 
+    /**
+     * Comprueba si el objeto se puede insertar en el inventario
+     * en caso de que asi sea lo inserta
+     * @param candidato Objeto que intentamos insertar en el inventario
+     * @param inventario Inventario donde intentamos insertar los objetos
+     * @return boolean para indicar si el candidato se puede insertar o no
+     */
     private static boolean esCandidatoFactible(Objeto candidato, Inventario inventario) {
         for (int j = 0; j < inventario.getN(); j++) {
             for (int i = 0; i < inventario.getN(); i++) {
@@ -30,10 +49,17 @@ public class Principal {
                 }
             }
         }
-
         return false;
     }
 
+    /**
+     * Funcion que determina si el objeto se puede introducir a partir de la celda indicada
+     * @param fil fila del inventario en el que nos encontramos
+     * @param col columna del inventario en el que nos encontramos
+     * @param objeto el objeto que queremos intentar insertar
+     * @param inventario el inventario donde queremos insertar el objeto
+     * @return boolean que indica si se puede insertar el objeto o no
+     */
     private static boolean celdaAceptable(int fil, int col, Objeto objeto, Inventario inventario) {
         int ancho = 0;
         int largo = 0;
@@ -50,23 +76,28 @@ public class Principal {
             else
                 break;
         }
-        return (objeto.getAlto() <= largo && objeto.getAncho() <= ancho);
+        return objeto.getAlto() <= largo && objeto.getAncho() <= ancho;
     }
 
-    private static void insertarObjeto(Inventario inventario, int i ,int j, Objeto candidato){
-        int auxj = j;
-        int auxi = i;
+    private static void insertarObjeto(Inventario inventario, int fil ,int col, Objeto candidato){
+        int auxCol = col;
+        int auxFil = fil;
 
-        while (auxi < candidato.getAncho() + i){
-            while (auxj < candidato.getAlto() + j){
-                inventario.setCelda(auxi, auxj, candidato.getId());
-                auxj++;
+        while (auxFil < candidato.getAncho() + fil){
+            while (auxCol < candidato.getAlto() + col){
+                inventario.setCelda(auxFil, auxCol, candidato.getId());
+                auxCol++;
             }
-            auxj = j;
-            auxi++;
+            auxCol = col;
+            auxFil++;
         }
     }
 
+    /**
+     * Algortimo de ordenacion Merge Sort
+     * Ordenamos en funcion del valor del objeto (a igual valor nos quedamos con el de menor tamaño)
+     * @param candidatos Array list con los objetos que queremos ordenar
+     */
     private static void mergeSort(ArrayList<Objeto> candidatos){
         mergeSortAux(candidatos, 0, candidatos.size()-1);
     }
@@ -87,9 +118,15 @@ public class Principal {
         int d = k+1;
         ArrayList<Objeto> aux = new ArrayList<Objeto>(in-i0+1);
         while(i<=k && d <= in)
-            if(candidatos.get(i).getValor() >= candidatos.get(d).getValor()){
-                aux.add(candidatos.get(i));
-                i++;
+            if(candidatos.get(i).getValor() > candidatos.get(d).getValor()){
+                if(candidatos.get(i).getValor() == candidatos.get(d).getValor() && (candidatos.get(i).getAncho()*candidatos.get(i).getAlto()) > (candidatos.get(d).getAncho()*candidatos.get(d).getAlto())){
+                    aux.add(candidatos.get(d));
+                    d++;
+                }
+                else{
+                    aux.add(candidatos.get(i));
+                    i++;
+                }
             }
             else{
                 aux.add(candidatos.get(d));
@@ -99,5 +136,4 @@ public class Principal {
         for (int b = d; b <= in ; b++) aux.add(candidatos.get(b));
         for (int c = 0; c < aux.size() ; c++) candidatos.set(c + i0, aux.get(c));
     }
-
 }
